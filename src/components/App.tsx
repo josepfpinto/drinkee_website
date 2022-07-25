c784552e00bd1e5b4ce8e79862ddd85e07153cd2
josepfpinto <josepfpinto@gmail.com>
2022-07-25 10:43:41 +0100

    fix http url

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -22,7 +22,7 @@ function App() {
                             <div className="site-header-inner">
                                 <div className="brand header-brand">
                                     <h1 className="m-0">
-                                        <a href="#">
+                                        <a href="https://github.com/josepfpinto">
                             <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                             <title>Laurel</title>
                                 <defs>
@@ -55,7 +55,7 @@ function App() {
                         <div className="hero-copy">
                                     <h1 className="hero-title mt-0">Drinkee App</h1>
                                     <p className="hero-paragraph">Our landing page template works on all devices, so you only have to set it up once, and get beautiful results forever.</p>
-                                    <div className="hero-cta"><a className="button button-shadow" href="#">Learn more</a><a className="button button-primary button-shadow" href="#">Early access</a></div>
+                                    <div className="hero-cta"><a className="button button-shadow" href="https://github.com/josepfpinto">Learn more</a><a className="button button-primary button-shadow" href="https://github.com/josepfpinto">Early access</a></div>
                         </div>
                         <div className="hero-app">
                         <div className="hero-app-illustration">
@@ -324,7 +324,7 @@ function App() {
                                             <input className="input" type="email" name="email" placeholder="Your best email&hellip;" />
                                         </div>
                                         <div className="control">
-                                            <a className="button button-primary button-block button-shadow" href="#">Early access</a>
+                                            <a className="button button-primary button-block button-shadow" href="https://github.com/josepfpinto">Early access</a>
                                         </div>
                                     </div>
                                 </div>
@@ -336,7 +336,7 @@ function App() {
                         <div className="container">
                             <div className="site-footer-inner has-top-divider">
                                 <div className="brand footer-brand">
-                                    <a href="#">
+                                    <a href="https://github.com/josepfpinto">
                         <svg width="32" height="32" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
                             <title>Laurel</title>
                             <defs>
@@ -359,21 +359,21 @@ function App() {
                                 </div>
                                 <ul className="footer-links list-reset">
                                     <li>
-                                        <a href="#">Contact</a>
+                                        <a href="https://github.com/josepfpinto">Contact</a>
                                     </li>
                                     <li>
-                                        <a href="#">About us</a>
+                                        <a href="https://github.com/josepfpinto">About us</a>
                                     </li>
                                     <li>
-                                        <a href="#">FAQ's</a>
+                                        <a href="https://github.com/josepfpinto">FAQ's</a>
                                     </li>
                                     <li>
-                                        <a href="#">Support</a>
+                                        <a href="https://github.com/josepfpinto">Support</a>
                                     </li>
                                 </ul>
                                 <ul className="footer-social-links list-reset">
                                     <li>
-                                        <a href="#">
+                                        <a href="https://github.com/josepfpinto">
                                             <span className="screen-reader-text">Facebook</span>
                                             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M6.023 16L6 9H3V6h3V4c0-2.7 1.672-4 4.08-4 1.153 0 2.144.086 2.433.124v2.821h-1.67c-1.31 0-1.563.623-1.563 1.536V6H13l-1 3H9.28v7H6.023z" fill="#FFF"/>
@@ -381,7 +381,7 @@ function App() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="#">
+                                        <a href="https://github.com/josepfpinto">
                                             <span className="screen-reader-text">Twitter</span>
                                             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z" fill="#FFF"/>
@@ -389,7 +389,7 @@ function App() {
                                         </a>
                                     </li>
                                     <li>
-                                        <a href="#">
+                                        <a href="https://github.com/josepfpinto">
                                             <span className="screen-reader-text">Google</span>
                                             <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg">
                                                 <path d="M7.9 7v2.4H12c-.2 1-1.2 3-4 3-2.4 0-4.3-2-4.3-4.4 0-2.4 2-4.4 4.3-4.4 1.4 0 2.3.6 2.8 1.1l1.9-1.8C11.5 1.7 9.9 1 8 1 4.1 1 1 4.1 1 8s3.1 7 7 7c4 0 6.7-2.8 6.7-6.8 0-.5 0-.8-.1-1.2H7.9z" fill="#FFF"/>
