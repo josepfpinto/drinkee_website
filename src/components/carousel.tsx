81f4a1ad83ef97749991128967f81fa61a21ba19
josepfpinto <josepfpinto@gmail.com>
2022-07-27 17:21:47 +0100

    carousel mobile

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -13,48 +13,101 @@ import start from '../images/screens/start.png';
 
 function FunctionalComponent() {
 
-    return (
-        <><div className=".SliderWrapper">
-            <Swiper
-                effect={"coverflow"}
-                grabCursor={true}
-                centeredSlides={true}
-                slidesPerView={3}
-                coverflowEffect={{
-                    rotate: 50,
-                    stretch: 0,
-                    depth: 50,
-                    modifier: 1,
-                    slideShadows: true,
-                    scale: 0.7,
-                }}
-                pagination={true}
-                navigation={true}
-                modules={[EffectCoverflow, Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>
-                    <img src={start} alt="start screen preview" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={homescreen} alt="homescreen screen preview" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={chat} alt="chat screen preview" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={friend} alt="friend screen preview" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={homescreen2} alt="homescreen screen preview" />
-                </SwiperSlide>
-                <SwiperSlide>
-                    <img src={profile} alt="profile screen preview" />
-                </SwiperSlide>
-            </Swiper>
+    const isMobile = window.innerWidth <= 650;
+
+    if (isMobile) {
+        return (
+            <><div className=".SliderWrapper">
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={1}
+                    initialSlide={1}
+                    slideNextClass={".nextCarousel"}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 50,
+                        modifier: 1,
+                        slideShadows: false,
+                        scale: 0.8,
+                    }}
+                    pagination={true}
+                    navigation={true}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src={start} alt="start screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={homescreen} alt="homescreen screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={chat} alt="chat screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={friend} alt="friend screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={homescreen2} alt="homescreen screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={profile} alt="profile screen preview" />
+                    </SwiperSlide>
+                </Swiper>
             </div>
-        </>
-    );
+            </>
+        );
+    } else {
+        return (
+            <><div className=".SliderWrapper">
+                <Swiper
+                    effect={"coverflow"}
+                    grabCursor={true}
+                    centeredSlides={true}
+                    slidesPerView={3}
+                    initialSlide={1}
+                    slideNextClass={".nextCarousel"}
+                    coverflowEffect={{
+                        rotate: 50,
+                        stretch: 0,
+                        depth: 50,
+                        modifier: 1,
+                        slideShadows: false,
+                        scale: 0.8,
+                    }}
+                    pagination={true}
+                    navigation={true}
+                    modules={[EffectCoverflow, Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide>
+                        <img src={start} alt="start screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={homescreen} alt="homescreen screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={chat} alt="chat screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={friend} alt="friend screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={homescreen2} alt="homescreen screen preview" />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={profile} alt="profile screen preview" />
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+            </>
+        );
+    }
+
+
 }
 
 export default FunctionalComponent;
